a910c835450f46ba9f65fb7dd2289c678742cca1
Santiago Montoya <santiago.mordz@gmail.com>
2024-10-18 15:11:55 +0000

    feat(database model): structured data base models for a Star Wars project

--- src/models.py
+++ src/models.py
@@ -1,29 +1,82 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum as SQLAlchemyEnum, DateTime, Float, Numeric, func 
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class CategoryType(enum.Enum):  
+    PLANETS = "planet"  
+    VEHICLES = "vehicle"  
+    CHARACTERS = "character"   
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_name = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False, unique=True)
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250), unique=True)
+    created_at = Column(DateTime, server_default=func.now())
+    favourites = relationship('Favourites', back_populates='user')
+
+class Login(Base):
+    __tablename__ = 'login'
+    id = Column(Integer, primary_key=True)
+    user_name= Column(String(250), nullable=False, unique=True)
+    password= Column(String(250), nullable=False, unique=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Favourites(Base):
+    __tablename__ = 'favourites'    
+    id = Column(Integer, primary_key=True)
+    type = Column(SQLAlchemyEnum(CategoryType), nullable=False)    
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    orbital_period = Column(Float)
+    population = Column(Float)
+    climate = Column(String(50))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    favourites = relationship('Favourites', back_populates='planet')
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    model = Column(String(50))
+    vehicle_class = Column(String(50))
+    manufacturer = Column(String(50))
+    cost_in_credits = Column(Numeric(6, 2))
+    length = Column(Numeric(5, 1))
+    crew = Column(Float)
+    passengers = Column(Float)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    favourites = relationship('Favourites', back_populates='vehicle')
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    orbital_period = Column(String(50))
+    population = Column(Integer)
+    climate = Column(String(50))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    favourites = relationship('Favourites', back_populates='character')
 
     def to_dict(self):
         return {}
